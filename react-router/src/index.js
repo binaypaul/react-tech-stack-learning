910e1197652d787adcf7736923206c360f693231
Binay Paul <binay.paul.1993@gmail.com>
2022-03-23 10:10:08 +0530

    Implemented Nested Route, ROute with param, NavLink

--- react-router/src/index.js
+++ react-router/src/index.js
@@ -5,14 +5,19 @@ import './index.css';
 import App from './App';
 import Invoices from './routes/Invoices';
 import Expenses from './routes/Expenses';
+import Invoice from './routes/Invoice';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path='/' element={<App />} >
-          <Route path='invoices' element={<Invoices /> }/>
+          <Route path='invoices' element={<Invoices /> }>
+            <Route index element={<p style={{padding: '1rem'}}>Select an Invoice to view</p>}/>
+            <Route path=':invoiceid' element={<Invoice />} />
+          </Route>
           <Route path='expenses' element={<Expenses />} />
+          <Route path='*' element={<main><p>No route match this url.!</p></main>}/>
         </Route>
       </Routes>
     </Router>
